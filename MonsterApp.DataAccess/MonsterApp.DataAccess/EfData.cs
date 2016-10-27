1e02278167d9dcfce33436bca5ebae86f78f086e
greg mckinney <gmck95@msn.com>
2016-10-27 16:15:27 -0400

    Connect database and add EF

---
+++ MonsterApp.DataAccess/MonsterApp.DataAccess/EfData.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterApp.DataAccess
+{
+  public class EfData
+  {
+
+    private MonsterDBEntities db = new MonsterDBEntities();
+
+
+    public List<Gender> GetGender()
+    {
+      return db.Genders.ToList();
+    }
+
+    public bool InsertGender(Gender gender)
+    {
+      db.Genders.Add(gender);
+
+      return db.SaveChanges() > 0;
+    }
+
+    public bool ChangeGender(Gender gender, EntityState state)
+    {
+      var entry = db.Entry<Gender>(gender);
+      entry.State = state;
+
+      return db.SaveChanges() > 0;
+    }
+
+    public void DeleteGender()
+    {
+
+    }
+
+
+
+    public List<MonsterType> GetMonsterType()
+    {
+      return db.MonsterTypes.ToList();
+    }
+
+    public bool InsertMonsterType(MonsterType type)
+    {
+      db.MonsterTypes.Add(type);
+
+      return db.SaveChanges() > 0;
+    }
+
+    public bool ChangeMonsterType(MonsterType type, EntityState state)
+    {
+      var entry = db.Entry<MonsterType>(type);
+      entry.State = state;
+
+      return db.SaveChanges() > 0;
+    }
+
+    public List<Title> GetTitle()
+    {
+      return db.Titles.ToList();
+    }
+
+    public bool InsertTitle(Title title)
+    {
+      db.Titles.Add(title);
+
+      return db.SaveChanges() > 0;
+    }
+
+    //public List<Title> GetMonster()
+    //{
+    //  return db.Monsters.ToList();
+    //}
+
+    //public void InsertMonster()
+    //{
+    //  db.Monsters.Add(monster);
+
+    //  return db.SaveChanges() > 0;
+    //}
+  }
+}
